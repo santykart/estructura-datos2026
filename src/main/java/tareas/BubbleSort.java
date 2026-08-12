a051b64921aad2886950292351d1be12f66e6fa6
Hugo Alexis <becerril.alexis2b@gmail.com>
2026-08-12 12:28:48 -0600

    Build: Activity 1 - Bubble Sort

--- src/main/java/tareas/BubbleSort.java
+++ src/main/java/tareas/BubbleSort.java
@@ -8,14 +8,29 @@ public class BubbleSort {
     MAIN DEL PROGRAMA
     ========================================= */
     public static void main(String[] args){
-        // Declarar arreglos 'A' y 'B'
-        int vendedores[] = {10};
-        int ventas[] = {45, 12, 78, 34, 56, 23, 91, 18, 67, 40};
+        // Declarar variables
+        Scanner sc = new Scanner(System.in);
+        int[] vendedores = {10};
+        int[] ventas = new int[10];
 
-        
+        // 
+
+        // Cerrar Scanner
+        sc.close();
     }
     
     /* =========================================
     MÉTODOS DEL PROGRAMA
     ========================================= */
+
+    // Método 1. Capturar ventas >> permite al usuario ingresar los valores de las 10 ventas realizadas
+    public void capturarVentas(){
+
+    }
+
+    // Método 2. Mostrar arreglo >> permite al usuario visualizar los valores de venta que ingresó en el 'Método 1'
+
+    // Método 3. Ordenar Burbuja >> permite al usuario reordenar los valores de menor a mayor
+
+    // Método 4. Calcular Promedio >> muestra al usuario el promedio de las ventas realizadas
 }
