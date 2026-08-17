626dd8aea0af142d95ecdbab8b3b921babce32c8
Hugo Alexis <becerril.alexis2b@gmail.com>
2026-08-17 09:10:12 -0600

    refactor: se agregan las clases 'Nodo' y 'ArrayStack'

--- src/main/java/actividadesclase/Main.java
+++ src/main/java/actividadesclase/Main.java
@@ -1,28 +1,43 @@
 package actividadesclase;
 
+import nodo.Nodo;
+import stack.ArrayStack;
+
 public class Main {
     public static void main(String[] args){
-        Nodo nodoUno = new Nodo(10);
-        Nodo nodoDos = new Nodo(20);
-        Nodo nodoTres = new Nodo(30);
+        // Declarar e inicializar variables
+        Nodo nodo = new Nodo(10);
+        Nodo nodo2 = new Nodo(20);
+        Nodo nodo3 = new Nodo(30);
 
-        System.out.println(".getNodo():");
-        System.out.println(nodoUno.getNodo());
-        System.out.println(nodoDos.getNodo());
-        System.out.println(nodoTres.getNodo());
+        // Enlazar nodos (setEnlace)
+        nodo.setEnlace(nodo2);
+        nodo2.setEnlace(nodo3);
 
-        System.out.println(".getSiguiente():");
-        System.out.println(nodoUno.getSiguiente());
-        System.out.println(nodoDos.getSiguiente());
-        System.out.println(nodoTres.getSiguiente());
-        
-        System.out.println(".setSiguiente():");
-        nodoUno.setSiguiente(nodoDos);
-        nodoDos.setSiguiente(nodoTres);
-        
-        System.out.println(".getSiguiente():");
-        System.out.println(nodoUno.getSiguiente());
-        System.out.println(nodoDos.getSiguiente());
-        System.out.println(nodoTres.getSiguiente());
+        // Apuntador
+        Nodo actual = nodo;
+
+        // 
+        while(actual.getEnalce() != null){
+            System.out.println(actual.getDato());
+            actual = actual.getEnalce();
+        }
+
+        // CLASE ARRAYSATCK
+        IO.println("\n<< ARRAYSTACK >>");
+        ArrayStack arrayStack = new ArrayStack(10);
+
+        arrayStack.push(10);
+        IO.println(arrayStack.toString());
+        arrayStack.push(20);
+        IO.println(arrayStack.toString());
+        arrayStack.push(30);
+        IO.println(arrayStack.toString() + "\n");
+
+        IO.println("cima " + arrayStack.peek());
+        IO.println("pop " + arrayStack.pop());
+        IO.println("cima " + arrayStack.peek());
+        IO.println(arrayStack.toString());
+
     }
 }
