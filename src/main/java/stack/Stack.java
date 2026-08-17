e27f99ad1ccf38783e0183d2bfaf37eecb5c122d
Hugo Alexis <becerril.alexis2b@gmail.com>
2026-08-17 14:12:33 -0600

    chore: reorganización de carpetas, archivo pom.xml y .gitignore

---
+++ src/main/java/stack/Stack.java
@@ -0,0 +1,16 @@
+package stack;
+
+public class Stack {
+    
+}
+
+/*
+    Asume el rol de un experto en Literaturas Mexicanas e Iberoamericanas de nivel bachillerato.
+
+Voy a realizar mi examen extraordinario de literatura, en el cual me solicitan leer el libro que te anexé. Quiero que realices los siguientes puntos:
+1. Una reseña detallada de todo el libro. Dime de qué se trata, los personajes principales y secundarios
+2. Quiero que me pongas en una lista a los personajes principales con sus características físicas, su forma de ser, el rol que tienen dentro de la trama, cómo se relacionan con otros personajes, etc. Dame todos sus detalles, incluyendo si tienen una forma de hablar o de vestir
+3. Quiero que realices una lista con los personajes secundarios y que incluyan todas las características que te solicité con los personajes principales
+4. Dame todos los momentos clave de la historia. Dime detalladamente lo que sucede, los personajes involucrados, el escenario en el que se encuentran, si van vestidos de alguna forma en específico, si algún personaje dice un diálogo que es relevante para la historia. 
+
+*/
