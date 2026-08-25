d87c341c6c9ae33fc00fa72c595d08bd7c59d792
Hugo Alexis <becerril.alexis2b@gmail.com>
2026-08-25 09:16:37 -0600

    Step 1-3: Upgrade project runtime to Java 25 and restore IO compatibility

---
+++ src/main/java/actividadesclase/IO.java
@@ -0,0 +1,15 @@
+package actividadesclase;
+
+public class IO {
+    public static void print(String value) {
+        System.out.print(value);
+    }
+
+    public static void println(String value) {
+        System.out.println(value);
+    }
+
+    public static void println() {
+        System.out.println();
+    }
+}
